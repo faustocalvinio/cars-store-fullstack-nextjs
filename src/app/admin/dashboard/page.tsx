729188b6470e0ed829433e8a8673b2268c2e7ead
Fausto Calviño <faustocalvino@outlook.com>
2025-04-11 13:17:47 +0200

    refactor: simplify dashboardPage component structure and improve readability

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,17 +1,12 @@
 import { AdminCarsList } from "@/component/car/AdminCarsList";
 
-
-export const dashboardPage = () => {
-  
-   
-
+export default function dashboardPage() {
    return (
       <>
          <h1>Dashboard</h1>
-         
-         <AdminCarsList  />
-         
+         <p>Welcome to the admin dashboard!</p>
+         <p>Here you can manage your cars.</p>
+         <AdminCarsList />
       </>
    );
-};
-export default dashboardPage;
+}
